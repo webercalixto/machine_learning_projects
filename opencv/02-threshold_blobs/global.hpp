f2be3e70b413b100774d5ca13db5879379e18320
Weber Calixto <webersouzacalixto@gmail.com>
2019-06-09 15:06:30 -0300

    Aplicativo opencv 02

---
+++ opencv/02-threshold_blobs/global.hpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include "opencv2/opencv.hpp"
+#include <opencv2/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
+
+
+#include "macros.hpp"
+
